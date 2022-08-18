2f24863be8e4df059063a82c6af34a932c43ecb0
rhthakur10215 <88919543+rhthakur10215@users.noreply.github.com>
2022-08-18 21:00:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 #import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import datetime
 
 import pandas_datareader as data
